0098e4e56daf9c672dd817c3f6e799027fbdf63a
hdytto <hdytto20824@gmail.com>
2016-02-24 21:53:45 +0800

    java

--- src/hello.java
+++ src/hello.java
@@ -3,7 +3,14 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 0;
+		
+		{
+			i = 1;
+		}
 		System.out.println("Hello World!");
+
+		System.out.println(i);
 
 	}
 
